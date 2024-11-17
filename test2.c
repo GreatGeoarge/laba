1ea977597faf991630f707391acff63d69b47d3b
GreatGeoarge <asaplog1996@gmail.com>
2024-11-17 19:29:17 +0300

    task number 44 lhl

--- test2.c
+++ test2.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <math.h>
 
+ double factorial(int n) {
+        double res = 1;
+        for(int i = 1; i <= n; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
 int main() {
-    long double x, y;
-    printf("Введите значение для x и y\n");
-    if(scanf("%Lf %Lf", &x, &y) != 2) {
-     printf("данные введены не коректно\n");
-     return 1;
-    } 
+    double x,e;
+    printf("Введите значение для x\n");
+    scanf("%lf", &x);
 
-    long double r = x*y;
+    printf("Введите значение для точности e ( 0 < e < 1)\n");
+    scanf("%lf", &e);
 
-    if(!isnan(r)) {
-          if(r < 1 && r > -1) 
-               printf("точка попала\n");
-           else 
-               printf("точка не попала\n");
-      } 
-      return 0;
-}
+    if(e <= 0 || e >= 1 ) {
+        printf("Данные введены неверно\n");
+        return 1;
+    }
+
+    double term = x; // перове слагаемое
+    double sum = term; // сумма ряда
+    int n = 0; 
+    int count = 1; // счетчик слагаемых 
+    
+    while(fabs(term) >= e) {
+        n++;
+        term = pow(-x, 2 * n + 1)/factorial(2 * n);
+        sum += term;
+        count++;
+    }
+
+    printf("Сумма ряда с точностью e = %lf\n", sum);
+    printf("Количество учтенных слогаемых = %d\n", count);
+    return 0;
+}  
+   
+
+
+
